386aa7b8fce80260e0cef51ac59e214bba58eba5
Melanius <melanie.vetsch@hotmail.com>
2016-12-03 17:04:42 +0100

    several amendments

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,43 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-Sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-    return gulp.src('./styles.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./'));
+    return gulp.src('assets/styles.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('assets'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch(['./styles.scss', './variablen.scss', './body', './list'] ['sass']);
-    gulp.watch('./_variablen.scss', ['sass']);
-    gulp.watch('./_body.scss', ['sass']);
-    gulp.watch('./_list.scss', ['sass']);
+    gulp.watch(['assets/styles.scss', 'assets/parials/*.scss'],['sass']);
+});
 
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch(['assets/styles.scss', 'assets/partials/*.scss'],['sass']);
+    gulp.watch(" *.html, 'assets/styles.scss', 'assets/partials/*.scss' ").on('change', browserSync.reload);
+});
+
+// Static server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
